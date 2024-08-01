5bfe37344411ca556be07c33b6a0478415b9cd37
Sibusiso <zulusibusiso81@gmail.com>
2024-08-02 01:39:07 +0200

    adding footer

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './Components/About/about'
 import Contact from './Components/Contact/Contact'
 import Education from './Components/Ed and certificates/education'
+import Footer from './Components/Footer/footer'
 import Home from './Components/Home/Home'
 import Projects from './Components/Projects/Projects'
 
@@ -15,6 +16,7 @@ function App() {
     <Projects/>
     <Education/>
     <Contact/>
+    <Footer/>
     </main>
   )
 }
--- Portfolio/src/Components/Contact/Contact.jsx
+++ Portfolio/src/Components/Contact/Contact.jsx
@@ -31,6 +31,7 @@ function Contact() {
     
   </div>
   </form>
+  
 </div>
 </section>
    
