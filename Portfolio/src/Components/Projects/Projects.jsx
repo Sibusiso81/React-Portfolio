298990265d4668b36e8cee0dd4fc5626739a3230
Sibusiso <zulusibusiso81@gmail.com>
2024-08-06 12:38:33 +0200

    spell corection on project section

--- Portfolio/src/Components/Projects/Projects.jsx
+++ Portfolio/src/Components/Projects/Projects.jsx
@@ -11,7 +11,7 @@ function Projects() {
             <div className='col-span-2 lg:col-span-1 flex flex-row justify-center lg:justify-center '>
             <div>
             <dotlottie-player  src="https://lottie.host/e8cd36b5-3318-41fb-b0cf-17c59c726f4d/QGOHqm7K5u.json" background="transparent" speed="1"  loop autoplay></dotlottie-player>
-            <h2 className='mt-2 text-center font-poppins'>A few lines away till complition...</h2>
+            <h2 className='mt-2 text-center font-poppins'>A few lines away till completion...</h2>
             </div>
             </div>
 
