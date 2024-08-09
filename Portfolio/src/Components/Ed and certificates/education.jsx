4e9a82ce9be91c44c0d3cfd66e2f18e8059ff0d8
Sibusiso81 <113394852+Sibusiso81@users.noreply.github.com>
2024-08-09 00:13:28 +0000

    updating ed and about sections

--- Portfolio/src/Components/Ed and certificates/education.jsx
+++ Portfolio/src/Components/Ed and certificates/education.jsx
@@ -6,7 +6,7 @@ function Education() {
   return (
     <section className='h-fit w-screen text-white font-poppins mx-auto flex flex-col items-center justify-center my-auto mt-10  lg:text-xl overflow-y-hidden'>
         <h1 className=' font-poppins font-semibold text-2xl lg:text-3xl text-center mx-auto p-5'>Education & Certifications</h1>
-    <div className='flex w-full h-full justify-center '>
+    <div className='flex w-full h-full  '>
     <div className='space-y-5 max-w-screen-sm '>
      
     <Card insitution={'Phoenix College of Johannesburg'} duration={'2020/09/12'} qualifiation={'Matric '}/>
